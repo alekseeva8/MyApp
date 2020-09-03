6bf1e23029e06cc7f49287b70477d48f5f155ad3
alekseeva8 <alekseeva.ev8@gmail.com>
2020-09-03 19:55:07 +0300

    initial commit, added .gitignore

--- WeatherApp/Forecast/ForecastViewController.swift
+++ WeatherApp/Forecast/ForecastViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ForecastViewController.swift
 //  WeatherApp
 //
 //  Created by Elena Alekseeva on 9/3/20.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ForecastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ WeatherApp/Today/TodayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TodayViewController.swift
+//  WeatherApp
+//
+//  Created by Elena Alekseeva on 9/3/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
